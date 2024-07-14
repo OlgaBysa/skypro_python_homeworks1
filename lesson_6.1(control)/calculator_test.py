cdf2449e2e52806fb14b67b07b6d5eac41ac8f07
Olga <Olyadusinka@gmail.com>
2024-07-14 10:47:09 +0300

    add first homework

--- lesson_6.1(control)/calculator_test.py
+++ lesson_6.1(control)/calculator_test.py
@@ -1,26 +1,47 @@
+import time
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 @pytest.fixture
-def browser():
+def chrome_browser():
     driver = webdriver.Chrome()
+    driver.maximize_window()
     yield driver
     driver.quit()
 
-def test_slow_calculator(browser):
-    browser.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
-    
-    delay_input = browser.find_element(By.CSS_SELECTOR, "#delay")
+
+def test_calculator_from(chrome_browser):
+    chrome_browser.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
+
+    delay_input = chrome_browser.find_element(By.ID, "delay")
     delay_input.clear()
-    delay_input.send_keys("45")
+    delay_input.send_keys(45)
+
+    chrome_browser.find_element(By.XPATH, "//span[text() = '7']").click()
+    chrome_browser.find_element(By.XPATH, "//span[text() = '+']").click()
+    chrome_browser.find_element(By.XPATH, "//span[text() = '8']").click()
+    chrome_browser.find_element(By.XPATH, "//span[text() = '=']").click()
     
-    keys_to_press = ["7", Keys.ADD, "8", Keys.ENTER]
-    result = "15"
+    WebDriverWait(chrome_browser, 46).until(EC.text_to_be_present_in_element((By.CLASS_NAME, "screen"), "15"))
+    result_text = chrome_browser.find_element(By.CLASS_NAME, "screen").text
+    assert result_text == "15"
+
+   
+
+# def test_calculator_from(chrom_browser):
+#     chrome_browser.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
     
-    for key in keys_to_press:
-        browser.find_element(By.CSS_SELECTOR, "#calculate").send_keys(key)  
+#     delay_input.clear()
+#     delay_input.send_keys(45)
+#     chrom_browser.find_element(By.XPATH, "//span[text() = '7']").click()
+#     chrom_browser.find_element(By.XPATH, "//span[text() = '+']").click()
+#     chrom_browser.find_element(By.XPATH, "//span[text() = '8']").click()
+#     chrom_browser.find_element(By.XPATH, "//span[text() = '=']").click()
     
-    output = browser.find_element(By.CSS_SELECTOR, "#display").text 
-    assert output == result, f"Expected result: {result}, Actual result: {output}"
+#     WebDriverWait(chrom_browser,46).until(EC.text_to_be_present_in_element((By.CLASS_NAME, "screen"), "15"))
+#     result_text = chrom_browser.find_element(By.CLASS_NAME, "screen").text
+#     assert result_text == "15"
